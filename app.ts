40ad58c440d2f50aeb49a4435ff1164a7de5e29e
joemc-immersify <117278037+joemc-immersify@users.noreply.github.com>
2024-01-26 09:08:53 +0000

    Update app.ts

--- app.ts
+++ app.ts
@@ -1,7 +1,6 @@
 import "reflect-metadata";
 import express from "express";
 import { AppDataSource } from "./src/data-source";
-import reviewsRoutes from "./src/routes/reviews";
 import customersRoutes from "./src/routes/customers";
 import productsRoutes from "./src/routes/products";
 
@@ -10,7 +9,6 @@ const port = 3000;
 
 app.use(express.json());
 
-app.use("/", reviewsRoutes);
 app.use("/", customersRoutes);
 app.use("/", productsRoutes);
 
