904d30482dad4bfb18dfdf79d89494acf2612516
fallenfreq <michael@catsdog.co.uk>
2024-02-12 05:29:44 +0000

    Update stock on purchase

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -14,20 +14,29 @@ router.get("/products", async (req, res) => {
         res.status(500).json({ error: "Internal Server Error" });
     }
 });
-//purchase product
-router.post("/purchaseProduct/:productId", async (req, res) => {
+
+// Purchase product
+router.post('/purchaseProduct/:productId', async (req, res) => {
     try {
-        const product = await AppDataSource.manager.findOne(Product, { where: { id: parseInt(req.params.productId) } });
-        if (product) {
-            await AppDataSource.manager.save(product);
-            res.json(product);
+      const product = await AppDataSource.manager.findOne(Product, {
+         where: { id: parseInt(req.params.productId) }
+        })
+  
+      if (product) {
+        if (product.stock > 0) {
+          product.stock -= 1
+          await AppDataSource.manager.save(product)
+          res.json(product)
         } else {
-            res.status(404).json({ error: "Product not found" });
+          res.status(422).json({ error: 'Product out of stock' })
         }
+      } else {
+        res.status(404).json({ error: 'Product not found' })
+      }
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Internal Server Error" });
+      console.error(error)
+      res.status(500).json({ error: 'Internal Server Error' })
     }
-});
+  })
 
 export default router;
