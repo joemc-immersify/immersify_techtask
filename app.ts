8cd6cc8b86d42e2b8b074a0597499367cd93ba0e
fallenfreq <michael@catsdog.co.uk>
2024-02-12 06:09:49 +0000

    POST route for creating reviews

--- app.ts
+++ app.ts
@@ -3,6 +3,7 @@ import express from "express";
 import { AppDataSource } from "./src/data-source";
 import customersRoutes from "./src/routes/customers";
 import productsRoutes from "./src/routes/products";
+import reviewsRoutes from "./src/routes/reviews";
 
 const app = express();
 const port = 3000;
@@ -11,6 +12,7 @@ app.use(express.json());
 
 app.use("/", customersRoutes);
 app.use("/", productsRoutes);
+app.use("/", reviewsRoutes);
 
 // Start the Express server
 app.listen(port, async () => {
