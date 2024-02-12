756e93ff47e7c0fe7d3e3f5d522d096abc98254f
fallenfreq <michael@catsdog.co.uk>
2024-02-12 06:23:09 +0000

    Add route to fetch reviews

--- src/routes/reviews.ts
+++ src/routes/reviews.ts
@@ -6,6 +6,14 @@ import { AppDataSource } from '../data-source'
 
 const router = express.Router()
 
+const isRatingValid = (rating: unknown): rating is string => {
+  return typeof rating === 'string' && /^[1-5]$/.test(rating);
+};
+
+const InvalidRatingError = {
+  error: 'Invalid rating. Must be a whole number between 1 and 5.'
+}
+
 // Route to add a new review
 router.post('/reviews', async (req: Request, res: Response) => {
   try {
@@ -14,8 +22,8 @@ router.post('/reviews', async (req: Request, res: Response) => {
     if (typeof review !== 'string')
       return res.status(400).json({ error: 'Review text is missing or invalid.' })
 
-    if (typeof rating !== 'string' || /^[1-5]$/.test(rating))
-      return res.status(400).json({error: 'Invalid rating. Must be a whole number between 1 and 5.'});
+    if (!isRatingValid(rating))
+      return res.status(400).json(InvalidRatingError);
 
     const product = await AppDataSource.manager.findOne(Product, { where: { id: parseInt(productId) }})
     const customer = await AppDataSource.manager.findOne(Customer, { where: { id: parseInt(customerId) }})
@@ -38,3 +46,27 @@ router.post('/reviews', async (req: Request, res: Response) => {
     res.status(500).json({ error: 'Internal Server Error' })
   }
 })
+
+// Route to get reviews of a certain rating for a given product
+router.get('/products/:productId/reviews', async (req: Request, res: Response) => {  
+  if (!isRatingValid(req.query.rating))
+    return res.status(400).json(InvalidRatingError);
+
+  try {
+    const reviews = await AppDataSource.manager.find(Review, {
+      where: {
+        product: { id: parseInt(req.params.productId) },
+        rating: parseInt(req.query.rating)
+      },
+      relations: ['customer', 'product']
+    });
+
+    res.json(reviews);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
+export default router
